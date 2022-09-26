6f8fa71e417a490b8338e78765e8566405d8b2f5
KKcorps <kharekartik@gmail.com>
2022-09-26 10:03:51 +0530

    Upgrade pinot version

--- scalar-function-example/src/main/java/org/apache/pinot/custom/function/CustomScalarFunctions.java
+++ scalar-function-example/src/main/java/org/apache/pinot/custom/function/CustomScalarFunctions.java
@@ -1,12 +1,12 @@
 package org.apache.pinot.custom.function;
 
 import com.github.davidmoten.geo.GeoHash;
-import org.apache.pinot.common.function.annotations.ScalarFunction;
+import org.apache.pinot.spi.annotations.ScalarFunction;
 
 
 public class CustomScalarFunctions {
 
-  @ScalarFunction(name = "encode_geohash")
+  @ScalarFunction(names = {"encode_geohash"})
   public static String latLngToGeo(Double latitude, Double longitude, Integer geoLength) {
     return GeoHash.encodeHash(latitude, longitude, geoLength);
   }
